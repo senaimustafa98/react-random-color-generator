4bacd3ee5ffce6b582e02de6cedff5647363d1c3
Senay Mustafa <senaim98@gmail.com>
2024-09-16 19:55:31 +0200

    Some changes to dependancies

--- src/App.js
+++ src/App.js
@@ -1,80 +1,89 @@
 import randomColor from 'randomcolor';
-import  { useState } from 'react';
-
-
-
+import { useState } from 'react';
 
 export default function App() {
   const initialColor = randomColor();
   const [color, setColor] = useState(initialColor);
-  const [isHovered, setIsHovered] = useState(false);
   console.log(initialColor);
-  return(
+  return (
     <>
-      <h1 style=
-      {{display:'flex', flexDirection: 'row', justifyContent: 'center', fontSize: '50px', margin: '35px', border: '5px'}}
-      >Random Color Generator</h1>
-      <div
-      style={{
-        fontFamily: 'sans-serif',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        fontSize: '35px',
-        margin: '50px 0px',
-      }}
+      <h1
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'center',
+          fontSize: '50px',
+          margin: '35px',
+          border: '5px',
+        }}
       >
-      </div>
+        Random Color Generator
+      </h1>
       <div
-      style={{
-        fontFamily: 'sans-serif',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        fontSize: '100px',
-        margin: '50px 0px',
-        backgroundColor: color,
-      }}>
+        style={{
+          fontFamily: 'sans-serif',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          fontSize: '35px',
+          margin: '50px 0px',
+        }}
+      />
+      <div
+        style={{
+          fontFamily: 'sans-serif',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          fontSize: '100px',
+          margin: '50px 0px',
+          backgroundColor: color,
+        }}
+      >
         <p>Generated Color:</p>
         {color}
       </div>
 
-<div
-style= {{
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  border: 'ridge black 10px',
-  borderRadius: '50px' ,
-  backgroundColor: 'pink',
-  justifyContent: 'center',
-  minWidth: '10em',
-  minHeight: '10em',
-  maxWidth: '350px',
-  margin: '0 auto',
-}}>
-
-
-  <button
-      style={{
-      width: '100%',
-      height: '100%',
-      margin: '0',
-      cursor: 'pointer',
-      fontFamily: 'sans-serif',
-      fontSize: '35px',
-      boxSizing: 'border-box',
-    }}
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          border: 'ridge black 10px',
+          borderRadius: '50px',
+          backgroundColor: 'pink',
+          justifyContent: 'center',
+          minWidth: '10em',
+          minHeight: '10em',
+          maxWidth: '350px',
+          margin: '0 auto',
+        }}
+      >
+        <button
+          style={{
+            width: '100%',
+            height: '100%',
+            margin: '0',
+            cursor: 'pointer',
+            fontFamily: 'sans-serif',
+            fontSize: '35px',
+            boxSizing: 'border-box',
+          }}
           onClick={() => {
-            setColor((randomColor));
+            setColor(randomColor);
           }}
         >
-          <p style={{
-            fontFamily: '-moz-initial',textAlign:'center', fontSize: '45px',
-          }}>Generate</p>
+          <p
+            style={{
+              fontFamily: '-moz-initial',
+              textAlign: 'center',
+              fontSize: '45px',
+            }}
+          >
+            Generate
+          </p>
         </button>
-</div>
+      </div>
     </>
-
   );
 }
